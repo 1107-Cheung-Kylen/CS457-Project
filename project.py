f9e942e89273ac110a603af9d42514a0d60fabf3
Kylen <kylenc@nevada.unr.edu>
2026-04-26 11:52:25 -0700

    initial commit

---
+++ project.py
@@ -0,0 +1,11 @@
+
+def main():
+    print("where have i been? [the app: by kylen cheung]")
+    print("options (plz pick 1)")
+    print("1. view cities") # allows to view all cities, or view my certain traits (like date, population, etc)
+    print("2. add cities") # add cities to database
+
+    # need class to store city objects
+    # maybe use array of city objects or something like that
+if __name__=="__main__":
+    main()
